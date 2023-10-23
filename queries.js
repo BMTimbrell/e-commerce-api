8dab6e2a93a9b46b84ecfb70ab42fdb721e352a5
Ben <bmtimbrell@gmail.com>
2023-10-23 15:30:46 +0100

    added products and products/id endpoints

--- queries.js
+++ queries.js
@@ -1,5 +1,6 @@
+const bcrypt = require('bcrypt');
 const Pool = require('pg').Pool;
-const pool = Pool({
+const pool = new Pool({
     user: 'postgres',
     host: 'localhost',
     database: 'shoe_shop',
@@ -7,6 +8,44 @@ const pool = Pool({
     port: 5432
 });
 
-/*export const getUsers = (request, response) => {
-    pool.query()
-}*/
+const getProducts = (request, response) => {
+    pool.query('SELECT * FROM shoes ORDER BY id', (error, results) => {
+        if (error) {
+            throw error;
+        }
+        response.status(200).json(results.rows);
+    });
+};
+
+const getProductById = (request, response) => {
+    const id = parseInt(request.params.id);
+
+    pool.query('SELECT * FROM shoes WHERE id = $1', [id], (error, results) => {
+        if (error) {
+            throw error;
+        }
+        response.status(200).json(results.rows);
+    });
+};
+
+const createUser = async (request, response) => {
+    const { name, email, password } = request.body;
+
+    const salt = await bcrypt.getSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    pool.query(
+        'INSERT INTO customers (name, email, password) VALUES ($1, $2, $3)', 
+        [name, email, hashedPassword], (error, results) => {
+            if (error) {
+                throw error;
+            }
+            response.status(201).send('Registration Complete!');
+        }
+    );
+};
+
+module.exports = {
+    getProducts,
+    getProductById
+};
