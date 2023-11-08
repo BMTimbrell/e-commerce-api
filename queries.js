d4f0161a49958153592727ca5bbd26999cf6f548
Ben <bmtimbrell@gmail.com>
2023-11-08 18:31:47 +0000

    changed shoes to have many to many relationship with sizes and adjusted shoe by id query

--- queries.js
+++ queries.js
@@ -58,7 +58,12 @@ const getProducts = (request, response) => {
 const getProductById = (request, response) => {
     const id = parseInt(request.params.id);
 
-    pool.query('SELECT * FROM shoes WHERE id = $1', [id], (error, results) => {
+    const query = 'SELECT shoes.id, shoes.name, shoes.manufacturer, shoes.category, shoes.price, shoes.gender, shoes.image, sizes.size'
+    + 'FROM shoes'
+    + 'INNER JOIN shoes_sizes ON shoes_sizes.shoe_id = shoes.id'
+    + 'INNER JOIN sizes ON sizes.id = shoes_sizes.size_id';
+
+    pool.query(`${query} WHERE shoes.id = $1`, [id], (error, results) => {
         if (error) {
             throw error;
         }
